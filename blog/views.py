c8a0ddb8be5d2c5a76037b5647a310b0d5ba14ca
NestVail <solidcopy105@gmail.com>
2022-02-16 11:47:32 +0900

    Remove @login_required (post_detail)

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,6 @@ def post_list(request):
 
 
 # 글 상세 정보
-@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
