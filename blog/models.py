296cefea7af8ace0dba443b03f4593f3bb1bb7f9
NestVail <solidcopy105@gmail.com>
2022-02-16 14:25:09 +0900

    add Comment class

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
         self.save()
 
     def __str__(self):
-        return self.text
+        return self.text + '(' + str(self.id) + ')'
 
 
 class Post(models.Model):
