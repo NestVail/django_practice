cc9cd3e703aec5021c4ebf543ed022c559d47915
NestVail <solidcopy105@gmail.com>
2022-02-16 09:44:53 +0900

    Add url (post_edit)

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # http://localhost:8000/blog/post/new
     path('post/new/', views.post_new, name='post_new'),
+    # http://localhost:8000/blog/post/(pk)/edit
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
